95495e8bbce5c63c335cd149301b5d65b0acd8ad
mabrsweeney <mabrsweeney@gmail.com>
2018-08-05 19:37:16 -0700

    added routes

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const port = 3000;
 
 app.use(express.static('client/public'));
 
+app.get('/immediate', (req, res) => {
+  console.log('got it!')
+  res.send('immediate!');
+});
+
+app.get('/wait', (req, res) => {
+  console.log('got it!')
+  setTimeout(function(){
+    res.send('wait!');
+  }, 1000);
+});
+
 app.listen(port, () => {
   console.log('Listening on localhost:', port);
 });
