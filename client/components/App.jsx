2d0023b1035c3a44204812e10cca74be6d7abab6
mabrsweeney <mabrsweeney@gmail.com>
2018-08-05 19:37:37 -0700

    added server requests

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import {get} from 'axios';
+import SubComponent from './SubComponent.jsx';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+    }
+  }
+
+  componentDidMount() {
+    get('/immediate')
+    .then((a,b) => {
+      console.log(a,b);
+    })
+  }
   render() {
-    return (<div>Hello, world!</div>)
+    return (<div id="app-container"><SubComponent /></div>)
   }
 }
